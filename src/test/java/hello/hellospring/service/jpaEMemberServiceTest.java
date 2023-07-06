e211c7b7bcfca09fdeb9bbc50154cf9552fd6a86
origuri <dlcksrb7@naver.com>
2023-07-06 23:58:06 +0900

    2023.7.6- jpa세팅과 테스트

---
+++ src/test/java/hello/hellospring/service/jpaEMemberServiceTest.java
@@ -0,0 +1,63 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@Transactional
+public class jpaEMemberServiceTest {
+   @Autowired
+   MemberService memberService;
+   @Autowired
+   MemberRepository memberRepository;
+
+
+
+    @Test
+    void 회원가입() {
+        // given
+        Member member = new Member();
+        member.setName("오리");
+
+        // when
+        Long saveId = memberService.join(member);
+
+        // then
+        Member findMember = memberService.findOne(saveId).get();
+        Assertions.assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    public void 중복회원예외(){
+        //given
+        Member member1 = new Member();
+        member1.setName("오리");
+
+        Member member2 = new Member();
+        member2.setName("오리");
+        //when
+        memberService.join(member1);
+        // junit 메소드 이 이셉션이 터져야 돼,  memberService.join(member2)가 되면
+        assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+
+        //then
+    }
+
+    @Test
+    void findMembers() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}
